f64aade9eb2fe3aff44d84ad155ea99852dae328
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-29 23:07:38 -0400

    Initial Commit

---
+++ uber clone/uber_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  uber_cloneApp.swift
+//  uber clone
+//
+//  Created by Silvanei  Martins on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct uber_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
