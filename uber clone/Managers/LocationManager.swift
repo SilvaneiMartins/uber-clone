7a75f3fc3bbfb46936c85d2b2e3a4f7dad648590
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-30 00:19:02 -0400

    Configuração do arquivo UberMapViewRepresentable

--- uber clone/Managers/LocationManager.swift
+++ uber clone/Managers/LocationManager.swift
@@ -3,6 +3,16 @@
 //  uber clone
 //  www.silvaneimartins.com.br
 //  Created by Silvanei  Martins on 29/09/22.
-//  silvaneimartins_rcc@hotmai.com
+//  silvaneimartins_rcc@hotmail.com
 
-import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
--- uber clone/UberMapViewRepresentable.swift
+++ uber clone/UberMapViewRepresentable.swift
@@ -12,6 +12,7 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     // MARK: - Propriedade
     
     let mapView = MKMapView()
+    let locationManager = LocationManager()
     
     // MARK: - Função
     func makeUIView(context: Context) -> some UIView {
@@ -33,13 +34,19 @@ struct UberMapViewRepresentable: UIViewRepresentable {
 
 // MARK: - Extensão
 extension UberMapViewRepresentable {
-    
     class MapCoordinator: NSObject, MKMapViewDelegate {
         let parent: UberMapViewRepresentable
         
         init(parent: UberMapViewRepresentable) {
             self.parent = parent
             super.init()
+        }
+        
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            )
+            
+            parent.mapView.setRegion(region, animated: true)
         }
     }
 }
